25ff669eecb5bee149d1339d0b985c25388b46b8
kiwor101 <111309721+kiwor101@users.noreply.github.com>
2026-04-27 10:13:04 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.FLUXO_SUPABASE_CONFIG = window.FLUXO_SUPABASE_CONFIG || {
   // Use apenas a publishable key ou anon key. Nunca use secret key ou service_role.
-  url: "",
-  anonKey: "",
+  url: "https://unxojwnhffdboccudwuy.supabase.co/",
+  anonKey: "sb_publishable_TxXCiVdX0tjYV1q9neZ4yw_OosFEjWm",
 };
